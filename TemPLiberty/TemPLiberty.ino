65c490adbc9142d4c18647cbf7f6a81d59816275
GitStudge <mjstudgeon@gmail.com>
2024-05-14 19:33:48 -0400

    Final revisions
    Added comments for the user's understanding

--- TemPLiberty/TemPLiberty.ino
+++ TemPLiberty/TemPLiberty.ino
@@ -1,21 +1,19 @@
-//Includes all libraries necessary for proper function.
+//Includes all libraries necessary for proper function
 #include <LiquidCrystal_I2C.h>
 #include <Adafruit_NeoPixel.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <Wire.h>
 
-
-#define LED_PIN 12      // Pin connected to the NeoPixels
+#define LED_PIN 12      // Pin connected to the LED Strip
 #define NUM_LEDS 5      // Number of NeoPixels
 #define BRIGHTNESS 200  // Set brightness (0 to 255)
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS 2  // Pin connected to Sensor
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-
 
 float Celsius = 0.0;
 float Fahrenheit = 0.0;
@@ -38,6 +36,7 @@ void setup() {
 
   lcd.init();
   lcd.backlight();
+  lcd.begin(20, 4);
 
   strip.begin();
   strip.setBrightness(BRIGHTNESS);
@@ -54,80 +53,74 @@ void loop() {
   lowerLimit = prevTemp - 1.0;
   upperLimit = prevTemp + 1.0;
 
-  lcd.begin(20, 4);
-
-
+  lcd.setCursor(3, 0);
+  lcd.print("Temp: " + String(Fahrenheit) + " F");
+  if(Fahrenheit < 100) {
+    lcd.setCursor(16, 0);
+    for (int i = 0; i < 3; i++) {
+      lcd.print(" ");
+    }
+  }
+  if(Fahrenheit >= 100) {
+    lcd.setCursor(17, 0);
+    for (int i = 0; i < 4; i++) {
+      lcd.print(" ");
+    }
+  }
   if (Fahrenheit < lowerLimit || Fahrenheit > upperLimit) {
-    lcd.clear();
-    lcd.setCursor(3, 0);
-    lcd.print("Temp: " + String(Fahrenheit) + " F");
-    lcd.setCursor(4, 1);
+    lcd.setCursor(5, 1);
     lcd.print("Sensing...");
-    for (int i = 0; i <= NUM_LEDS; i++) {
-      strip.setPixelColor(i, strip.Color(0,0,0));
-      strip.show();
-    }
 
-    for (int i = 0; i <= NUM_LEDS; i++) {
-      strip.setPixelColor(i, strip.Color(255, 150, 0));
-      if (i > 0) {
-        strip.setPixelColor(i - 1, strip.Color(0, 0, 0));
-      }
+    for(int i = 0; i <= NUM_LEDS; i++) {
+      strip.setPixelColor(i, strip.Color(0, 0, 0));
       strip.show();
-      delay(250);
     }
-    for (int i = 0; i <= NUM_LEDS; i++) {
-      strip.setPixelColor(i, strip.Color(0,0,0));
+    for(int i = 0; i <= NUM_LEDS; i++) {
+      strip.setPixelColor(i, strip.Color(255, 150, 0));
+      delay(250);
+      strip.show();
+      strip.setPixelColor(i, strip.Color(0, 0, 0));
+      delay(200);
       strip.show();
     }
   }
 
-
-if (Fahrenheit >= lowerLimit && Fahrenheit <= upperLimit) {
+if (Fahrenheit >= lowerLimit && Fahrenheit <= upperLimit) { //Checking to see if temperature is stable
   if (Fahrenheit < 98) {
     for (int i = 0; i <= NUM_LEDS; i++) {
       strip.setPixelColor(i, strip.Color(0, 0, 255));
       strip.show();
     }
-    lcd.clear();
-    lcd.setCursor(3, 0);
-    lcd.print("Temp: " + String(Fahrenheit) + " F");
     lcd.setCursor(5, 1);
     lcd.print("Cold Water");
   }
+
   if (Fahrenheit > 98 && Fahrenheit < 106) {
     for (int i = 0; i <= NUM_LEDS; i++) {
       strip.setPixelColor(i, strip.Color(0, 255, 0));
       strip.show();
     }
-    lcd.clear();
-    lcd.setCursor(3, 0);
-    lcd.print("Temp: " + String(Fahrenheit) + " F");
     lcd.setCursor(5, 1);
     lcd.print("Warm Water");
   }
+
   if (Fahrenheit > 106 && Fahrenheit < 109) {
     for (int i = 0; i <= NUM_LEDS; i++) {
       strip.setPixelColor(i, strip.Color(255, 255, 0));
       strip.show();
     }
-    lcd.clear();
-    lcd.setCursor(3, 0);
-    lcd.print("Temp: " + String(Fahrenheit) + " F");
     lcd.setCursor(5, 1);
     lcd.print("Almost Hot");
   }
+
   if (Fahrenheit > 109) {
     for (int i = 0; i <= NUM_LEDS; i++) {
       strip.setPixelColor(i, strip.Color(255, 0, 0));
       strip.show();
     }
-    lcd.clear();
-    lcd.setCursor(3, 0);
-    lcd.print("Temp: " + String(Fahrenheit) + " F");
     lcd.setCursor(5, 1);
     lcd.print("Hot Water!");
   }
 }
-prevTemp = Fahrenheit;
+prevTemp = Fahrenheit; // Updating the previous temperature
 }
